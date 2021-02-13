0d1637cc234d7735e55fb8080de022313c267ed3
Filippo Arduini <filippoarduini832@gmail.com>
2021-02-13 20:57:48 +0100

    added dependences

--- pages/index.js
+++ pages/index.js
@@ -17,21 +17,21 @@ export default function Home() {
   const sendNotification = () => {
     if (Notification.permission == "granted") {
       navigator.serviceWorker.getRegistration().then(function (reg) {
-              const options = {
-        body: 'Here is a notification body!',
-        icon: 'https://www.flaticon.com/svg/vstatic/svg/606/606795.svg?token=exp=1613242192~hmac=733b77a1a9b1798f564968528bb392ec',
-        vibrate: [100, 50, 100],
-        data: {
-          dateOfArrival: Date.now(),
-          primaryKey: 1
-        },
-        actions: [
-          {action: 'explore', title: 'Explore this new world'},
-          {action: 'close', title: 'Close notification'}
-        ]
-      };
+        const options = {
+          body: 'Here is a notification body!',
+          icon: 'https://www.flaticon.com/svg/vstatic/svg/606/606795.svg?token=exp=1613242192~hmac=733b77a1a9b1798f564968528bb392ec',
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+          },
+          actions: [
+            {action: 'explore', title: 'Explore this new world'},
+            {action: 'close', title: 'Close notification'}
+          ]
+        };
 
-        reg.showNotification('Hello world!',options);
+        reg.showNotification('Hello world!', options);
       });
 
     }
