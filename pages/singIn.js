9c44097c1cee577a72d98f64e33d4ec872adf913
Filippo Arduini <filippoarduini832@gmail.com>
2021-02-14 19:30:37 +0100

    added todo list

--- pages/singIn.js
+++ pages/singIn.js
@@ -1,7 +1,12 @@
+import { TextField } from "@material-ui/core"
+
 export default function SingIn(){
   return(
-    <div>
-      ciao da sing
+    <div className="w-full bg-gray-400">
+      <input placeholder="nome"></input>
+      <input placeholder="cognome"></input>
+      <input placeholder="email"></input>
+      <input placeholder="password"></input>
     </div>
   )
 
