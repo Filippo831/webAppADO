25118db901452acfd788f8c203be62b779d22b36
Filippo Arduini <filippoarduini832@gmail.com>
2021-03-11 10:36:06 +0100

    new events

--- pages/newEvent.js
+++ pages/newEvent.js
@@ -1,5 +1,6 @@
-import {Input, Button, NumberInput, NumberInputField} from "@chakra-ui/react"
+import {Input, Button, NumberInput, NumberInputField, Textarea} from "@chakra-ui/react"
 import {useState} from "react"
+import Navbar from "./components/main/navbar/navbar"
 
 export default function NewEvent() {
   const [data, setDate] = useState()
@@ -8,13 +9,17 @@ export default function NewEvent() {
   const [descrizione, setDescrizione] = useState()
 
   return (
-    <div className="h-screen">
+    <div>
+      <Navbar></Navbar>
+    <div className="w-5/6 h-screen mx-auto "
+
+      style={{
+        maxWidth: "400px"
+      }}
+    >
 
       {/* --- INPUTS ---*/}
-      <div className="flex flex-col justify-between w-5/6 pt-10 mx-auto h-3/5"
-        style={{
-          maxWidth:"400px"
-        }}
+      <div className="flex flex-col justify-between pt-10 h-3/5"
       >
 
         {/* --- DATE ---*/}
@@ -32,8 +37,15 @@ export default function NewEvent() {
 
         <Input placeholder="titolo"></Input>
         <Input placeholder="categoria"></Input>
-        <Input placeholder="descrizione"></Input>
+        <Textarea placeholder="descrizione"></Textarea>
+      </div>
+      <div className="flex flex-col justify-around my-auto h-1/4">
+        <div className="flex justify-around ">
+          <Button colorScheme="teal">annulla</Button>
+          <Button colorScheme="teal">conferma</Button>
+        </div>
       </div>
     </div>
+</div>
   )
 }
